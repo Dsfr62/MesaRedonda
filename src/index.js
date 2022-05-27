4e68292fa745bb61390357eb30dd3c92982f670e
Dsfr62 <saviodudu79@gmail.com>
2022-05-27 16:08:28 -0300

    Register post working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 
+let users = [];
+
+app.use(express.json());
+
 app.get('/', function (req, res) {
     res.send('Hello world!');
+});
+
+app.post('/cadastro/', function (req, res) {
+    const user = req.body;
+    if (!user.name) {
+        res.status(400).send('Put a valid name.');
+        return ;
+    };
+    users.push(user);
+    console.log(`${user.name} was created!`);
+    res.send(`${user.name} was created!`);
 });
 
 module.exports = app;
